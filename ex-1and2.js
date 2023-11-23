b4e84e2c5cb8e6f687d94142d7e0a4b66cafc297
Nok <siriwan.skk@gmail.com>
2023-11-23 15:32:01 +0700

    feat: ex1 finish

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstname = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantname = "Chef's Table by Chef James";
+const totalprice = 15000;
+const numberofrestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalprice = 5000;
+numberofrestaurant = 2500;
